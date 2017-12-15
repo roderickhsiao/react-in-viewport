42dd97dd125315a0fa28f53c158f2513356f29b1
Roderick Hsiao <roderickhsiao@gmail.com>
2017-12-15 00:14:20 -0800

    Real es module

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -9,7 +9,11 @@
 echo "=> Transpiling 'src' into ES5 ..."
 echo ""
 rm -rf ./dist
-NODE_ENV=production ./node_modules/.bin/babel --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist
+NODE_ENV=production ./node_modules/.bin/babel --no-babelrc --ignore tests,stories --plugins "transform-runtime" --presets react-app ./src --out-dir ./dist
+
+echo "=> Transpiling 'src' into ES6 ..."
+NODE_ENV=production BABLE_ENV=es ./node_modules/.bin/babel --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist/es
+
 echo ""
 echo "=> Transpiling completed."
 
