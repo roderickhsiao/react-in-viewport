04833fa2e5a2aa9513f9fe7d3bcfc90876ff8f5f
Roderick Hsiao <roderickhsiao@gmail.com>
2019-03-05 22:52:55 -0800

    Comment out unused plugin

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment, PureComponent } from 'react';
-import { storiesOf, setAddon } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import AspectRatio from 'react-aspect-ratio';
