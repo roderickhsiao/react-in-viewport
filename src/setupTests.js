288bfff14b018ccb5297d881ebe89842993b5745
Roderick Hsiao <roderickhsiao@gmail.com>
2019-04-03 21:37:24 -0700

    Don't publish storybook and test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+if (typeof window !== 'undefined') {
+  // Polyfills for intersection-observer
+  require('intersection-observer'); // eslint-disable-line
+}
