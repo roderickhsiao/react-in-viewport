98cd7671cdcf09739b8e2dcaf00361ca9803478e
Roderick Hsiao <roderickhsiao@users.noreply.github.com>
2019-09-05 13:32:26 -0700

    Only disconnect on leave (#42)
    * Only disconnect on leave

--- src/lib/useInViewport.js
+++ src/lib/useInViewport.js
@@ -45,7 +45,7 @@ const useInViewport = (target, options, config = { disconnectOnLeave: false }, p
       onLeaveViewport && onLeaveViewport();
       if (config.disconnectOnLeave) {
         // disconnect obsever on leave
-        stopObserver();
+        observer.current.disconnect();
       }
       setInViewport(isInViewport);
       setLeaveCount(leaveCount + 1);
