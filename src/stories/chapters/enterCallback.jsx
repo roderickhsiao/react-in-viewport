9b6e17aff5a9d65e4309180bb6ceb712313535a9
Roderick Hsiao <roderick.hsiao@gotinder.com>
2019-11-15 18:46:02 -0800

    Add example

--- src/stories/chapters/enterCallback.jsx
+++ src/stories/chapters/enterCallback.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import omit from 'lodash/omit';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import handleViewport, { customProps } from '../../index';
+import { handleViewport, customProps } from '../../index';
 import { Block } from '../common/themeComponent';
+import RefBlock from '../common/ref';
 
 const ViewportBlock = handleViewport(Block, {}, { disconnectOnLeave: false });
+const ViewportBlockThreshold = handleViewport(
+  Block,
+  { threshold: [0, 1.0] },
+  { disconnectOnLeave: false }
+);
+
 const CustomAnchor = ({ forwardedRef, inViewport, ...restProps }) => {
   const text = inViewport ? 'Link (in viewport)' : 'Link (not in viewport)';
   return (
@@ -14,7 +22,16 @@ const CustomAnchor = ({ forwardedRef, inViewport, ...restProps }) => {
       href="https://github.com/roderickhsiao/react-in-viewport#readme"
       {...omit(restProps, customProps)}
       ref={forwardedRef}
-      style={{ padding: '20px 0' }}
+      style={{
+        padding: '20px 0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '400px',
+        height: '300px',
+        backgroundColor: inViewport ? '#217ac0' : '#ff9800',
+        color: '#fff'
+      }}
     >
       {text}
     </a>
@@ -53,6 +70,45 @@ storiesOf('Enter and Leave callback', module)
               <ViewportAnchor
                 onEnterViewport={() => action('callback')('onEnterViewport')}
                 onLeaveViewport={() => action('callback')('onLeaveViewport')}
+              />
+            )
+          }
+        ]
+      }
+    ]
+  })
+  .addWithChapters('Ref component', {
+    subtitle: 'Multiple layer HOC',
+    chapters: [
+      {
+        sections: [
+          {
+            title: 'onEnterViewport and onLeaveViewport callback',
+            sectionFn: () => (
+              <Router>
+                <RefBlock
+                  onEnterViewport={() => action('callback')('onEnterViewport')}
+                  onLeaveViewport={() => action('callback')('onLeaveViewport')}
+                />
+              </Router>
+            )
+          }
+        ]
+      }
+    ]
+  })
+  .addWithChapters('Threshold', {
+    subtitle: 'Set different threshold',
+    chapters: [
+      {
+        sections: [
+          {
+            title: 'onEnterViewport and onLeaveViewport callback',
+            subtitle: 'threshold 1.0',
+            sectionFn: () => (
+              <ViewportBlockThreshold
+                onEnterViewport={() => action('callbackWithThreshold')('onEnterViewport')}
+                onLeaveViewport={() => action('callbackWithThreshold')('onLeaveViewport')}
               />
             )
           }
