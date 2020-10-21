bda484351e0892a9c7f94daac707a93e09bf4085
Roderick Hsiao <roderickhsiao@gmail.com>
2020-10-20 18:48:59 -0700

    Remove enzyme adaptor

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,5 @@
 // setup file
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 if (typeof window !== 'undefined') {
   // Polyfills for intersection-observer
