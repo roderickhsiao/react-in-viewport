0f37213b7ea1d062eab24fbbadf8168aec0a66e8
Roderick Hsiao <roderickhsiao@gmail.com>
2020-10-20 18:58:02 -0700

    Remove config

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,4 @@
 // setup file
-import { configure } from 'enzyme';
-
 if (typeof window !== 'undefined') {
   // Polyfills for intersection-observer
   require('intersection-observer'); // eslint-disable-line
