860740557459ca2903b1236a02b295e8d41172f0
Roderick Hsiao <roderickhsiao@gmail.com>
2020-10-20 19:03:51 -0700

    Enzyme hasn't support React 17

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
 // setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 if (typeof window !== 'undefined') {
   // Polyfills for intersection-observer
   require('intersection-observer'); // eslint-disable-line
