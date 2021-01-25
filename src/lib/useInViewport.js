c7f701ef95c627dc0eaa6083c4fd1fad9dacea87
Olierook <Robbert_olierook@hotmail.com>
2021-01-25 10:45:23 +0100

    Update useInViewport.js
    Remove finddomnode, let the hook return the 'getNode' function to add as ref for the component

--- src/lib/useInViewport.js
+++ src/lib/useInViewport.js
@@ -1,93 +1,92 @@
-// React hooks
-import { useEffect, useRef, useState } from 'react';
-import { findDOMNode } from 'react-dom';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-const useInViewport = (target, options, config = { disconnectOnLeave: false }, props) => {
-  const { onEnterViewport, onLeaveViewport } = props;
-  const [, forceUpdate] = useState();
+const useInViewport = (props, options, config = { disconnectOnLeave: false }) => {
+	const [node, setNode] = useState(null);
+	const getNode = useCallback(myNode => {
+		if (myNode !== null) {
+			setNode(node);
+		}
+	}, []);
+	const { onEnterViewport, onLeaveViewport } = props;
+	const [, forceUpdate] = useState();
 
-  const observer = useRef();
+	const observer = useRef();
 
-  const inViewportRef = useRef(false);
-  const intersected = useRef(false);
+	const inViewportRef = useRef(false);
+	const intersected = useRef(false);
 
-  const enterCountRef = useRef(0);
-  const leaveCountRef = useRef(0);
+	const enterCountRef = useRef(0);
+	const leaveCountRef = useRef(0);
 
-  function startObserver() {
-    if (target.current && observer.current) {
-      const node = findDOMNode(target.current);
-      if (node) {
-        observer.current.observe(node);
-      }
-    }
-  }
+	function startObserver() {
+		if (node && observer.current) {
+			observer.current.observe(node);
+		}
+	}
 
-  function stopObserver() {
-    if (target.current && observer.current) {
-      const node = findDOMNode(target.current);
-      if (node) {
-        observer.current.unobserve(node);
-        observer.current.disconnect();
-        observer.current = null;
-      }
-    }
-  }
+	function stopObserver() {
+		if (node && observer.current) {
+			observer.current.unobserve(node);
+			observer.current.disconnect();
+			observer.current = null;
+		}
+	}
 
-  function handleIntersection(entries) {
-    const entry = entries[0] || {};
-    const { isIntersecting, intersectionRatio } = entry;
-    const isInViewport = typeof isIntersecting !== 'undefined' ? isIntersecting : intersectionRatio > 0;
+	function handleIntersection(entries) {
+		const entry = entries[0] || {};
+		const { isIntersecting, intersectionRatio } = entry;
+		const isInViewport = typeof isIntersecting !== "undefined" ? isIntersecting : intersectionRatio > 0;
 
-    // enter
-    if (!intersected.current && isInViewport) {
-      intersected.current = true;
-      onEnterViewport && onEnterViewport();
-      enterCountRef.current += 1;
-      inViewportRef.current = isInViewport;
-      forceUpdate(isInViewport);
-      return;
-    }
+		// enter
+		if (!intersected.current && isInViewport) {
+			intersected.current = true;
+			onEnterViewport && onEnterViewport();
+			enterCountRef.current += 1;
+			inViewportRef.current = isInViewport;
+			forceUpdate(isInViewport);
+			return;
+		}
 
-    // leave
-    if (intersected.current && !isInViewport) {
-      intersected.current = false;
-      onLeaveViewport && onLeaveViewport();
-      if (config.disconnectOnLeave && observer.current) {
-        // disconnect obsever on leave
-        observer.current.disconnect();
-      }
-      leaveCountRef.current += 1;
-      inViewportRef.current = isInViewport;
-      forceUpdate(isInViewport);
-    }
-  }
+		// leave
+		if (intersected.current && !isInViewport) {
+			intersected.current = false;
+			onLeaveViewport && onLeaveViewport();
+			if (config.disconnectOnLeave && observer.current) {
+				// disconnect obsever on leave
+				observer.current.disconnect();
+			}
+			leaveCountRef.current += 1;
+			inViewportRef.current = isInViewport;
+			forceUpdate(isInViewport);
+		}
+	}
 
-  function initIntersectionObserver() {
-    if (!observer.current) {
-      // $FlowFixMe
-      observer.current = new IntersectionObserver(handleIntersection, options);
-    }
-  }
+	function initIntersectionObserver() {
+		if (!observer.current) {
+			// $FlowFixMe
+			observer.current = new IntersectionObserver(handleIntersection, options);
+		}
+	}
 
-  useEffect(
-    () => {
-      // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-      initIntersectionObserver();
-      startObserver();
+	useEffect(
+		() => {
+			// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+			initIntersectionObserver();
+			startObserver();
 
-      return () => {
-        stopObserver();
-      };
-    },
-    [target.current, options, config, onEnterViewport, onLeaveViewport]
-  );
+			return () => {
+				stopObserver();
+			};
+		},
+		[node, options, config, onEnterViewport, onLeaveViewport]
+	);
 
-  return {
-    inViewport: inViewportRef.current,
-    enterCount: enterCountRef.current,
-    leaveCount: leaveCountRef.current
-  };
+	return {
+		inViewport: inViewportRef.current,
+		enterCount: enterCountRef.current,
+		leaveCount: leaveCountRef.current,
+		getNode
+	};
 };
 
 export default useInViewport;
